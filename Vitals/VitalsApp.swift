b797121aaed2327a986cc51c8b58cd1008317c86
Samuel Majtan <56265039+sammyeatworld@users.noreply.github.com>
2026-03-03 22:44:14 +0100

    Initial commit

---
+++ Vitals/VitalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  VitalsApp.swift
+//  Vitals
+//
+//  Created by Samuel Majtan on 03/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct VitalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VitalsTests/VitalsTests.swift
@@ -0,0 +1,17 @@
+//
+//  VitalsTests.swift
+//  VitalsTests
+//
+//  Created by Samuel Majtan on 03/03/2026.
+//
+
+import Testing
+@testable import Vitals
+
+struct VitalsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
