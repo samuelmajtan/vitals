ac16d2e1056df7ece7348c8264b473187d2454bb
Samuel Majtan <56265039+samuelmajtan@users.noreply.github.com>
2026-03-20 22:17:27 +0100

    Vitals: Add chevron button

---
+++ Vitals/Components/Buttons/ChevronButton.swift
@@ -0,0 +1,60 @@
+//
+// Copyright (c) 2026, Samuel Majtan
+//
+// SPDX-License-Identifier: GPL-3.0
+//
+
+import SwiftUI
+
+struct ChevronButton: View {
+    
+    // MARK: - Properties
+    
+    private let title: String
+    private let image: String?
+    private let color: Color?
+    private let action: () -> Void
+    
+    // MARK: - Lifecycle
+    
+    init(
+        _ title: String,
+        image: String,
+        color: Color? = nil,
+        action: @escaping () -> Void
+    ) {
+        self.title = title
+        self.image = image
+        self.color = color
+        self.action = action
+    }
+    
+    // MARK: - View
+    
+    var body: some View {
+        Button(action: action) {
+            HStack(alignment: .center) {
+                HStack(alignment: .firstTextBaseline) {
+                    if let image {
+                        Image(systemName: image)
+                            .foregroundStyle(color ?? .accent)
+                    }
+                    Text(title)
+                        .foregroundStyle(.primary)
+                }
+                Spacer()
+                Image(systemName: "chevron.right")
+                    .foregroundStyle(.gray)
+            }
+        }
+    }
+    
+}
+
+// MARK: - Preview
+
+#Preview {
+    ChevronButton("Title", image: "heart", color: .blue) {
+        print("Navigating to...")
+    }
+}
