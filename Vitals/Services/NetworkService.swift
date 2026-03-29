cbca8deba1214a85b164e3f5ff78dba4c704394e
Samuel Majtan <56265039+samuelmajtan@users.noreply.github.com>
2026-03-29 03:49:35 +0200

    Vitals: Base implementation of NetworkService

--- Vitals/Services/NetworkService.swift
+++ Vitals/Services/NetworkService.swift
@@ -5,3 +5,54 @@
 //
 
 import Foundation
+import FactoryKit
+
+// MARK: - Protocol
+
+protocol NetworkingProtocol: AnyObject {
+
+    // MARK: - Properties
+
+    // MARK: - Methods
+
+}
+
+// MARK: - Implementation
+
+final class NetworkingService: NetworkingProtocol {
+    
+    // MARK: - Properties
+    
+    private let session : URLSession
+    
+    // MARK: - Initializer
+    
+    init(session: URLSession = URLSession(configuration: .default)) {
+        self.session = session
+    }
+    
+}
+
+// MARK: - Public Methods
+
+extension NetworkingService {
+    
+}
+
+// MARK: - Private Methods
+
+private extension NetworkingService {
+
+}
+
+// MARK: - Factory
+
+extension Container {
+
+    var networkingService: Factory<NetworkingProtocol> {
+        self { @MainActor in NetworkingService() }
+            .singleton
+    }
+
+}
+
